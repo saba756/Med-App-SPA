a8a25cbddd144555d32caec8f0c32fc2172de9a2
muhammadumerfarooq <umer.farooq6028@gmail.com>
2020-11-20 05:05:10 +0500

    added navs, footer, header and layouts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import {ComponentsModule} from './components/components.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RouterModule } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent,
-    HomeComponent,
-    NavBarComponent,
-    LoginComponent,
-    RegisterComponent
+    AdminLayoutComponent,
   ],
   imports: [
+    FormsModule,
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    NgbModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/side-bar/side-bar.component.ts
+++ client/src/app/components/side-bar/side-bar.component.ts
@@ -1,5 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
+  { path: '/icons', title: 'Icons',  icon:'education_atom', class: '' },
+  { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
+  { path: '/notifications', title: 'Notifications',  icon:'ui-1_bell-53', class: '' },
+
+  { path: '/user-profile', title: 'User Profile',  icon:'users_single-02', class: '' },
+  { path: '/table-list', title: 'Table List',  icon:'design_bullet-list-67', class: '' },
+  { path: '/typography', title: 'Typography',  icon:'text_caps-small', class: '' },
+  { path: '/upgrade', title: 'Upgrade to PRO',  icon:'objects_spaceship', class: 'active active-pro' }
+
+];
+
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
@@ -7,9 +26,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  menuItems: any[];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
-
+  isMobileMenu() {
+      if ( window.innerWidth > 991) {
+          return false;
+      }
+      return true;
+  };
 }
